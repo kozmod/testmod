a4b9924cd3ef1a5ba81f8ddf5a36b83d2c725620
18388871 <Koz00mod00>
2020-03-08 20:47:41 +0300

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kozmod/testmod
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,12 @@
+package testmod
+
+import "fmt" 
+
+// Hi returns a friendly greeting
+func Hi(name string) string {
+   return fmt.Sprintf("Hi, %s", name)
+}
+
+//func Hi2(name string) string {
+//   return fmt.Sprintf("Hi2, %s", name)
+//}
